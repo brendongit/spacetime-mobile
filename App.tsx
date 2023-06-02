0baf88397a7a8de9af18be5a42c02e3e5267c88e
brendongit <brenddesigneroficial@gmail.com>
2023-06-02 13:01:49 -0300

    button

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { ImageBackground, Text, View } from "react-native";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 import blurBg from "./src/assets/bg-blur.png";
 import Stripes from "./src/assets/stripes.svg";
 import NLWLogo from "./src/assets/nlw-spacetime-logo.svg";
@@ -29,7 +29,7 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="relative flex-1 items-center justify-center bg-gray-900"
+      className="relative flex-1 items-center justify-center bg-gray-900 px-8 py-10"
       imageStyle={{ position: "absolute", left: "-100%" }}
     >
       <StyledStripes className="absolute left-2" />
@@ -44,10 +44,20 @@ export default function App() {
             quiser) com o mundo!
           </Text>
         </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
       </View>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        © Copyright - Rocketseat
+      </Text>
       <StatusBar style="light" translucent />
     </ImageBackground>
   );
 }
-
-// aula 2 - 1:09:00
